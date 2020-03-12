3a1558e240cb78c3c77ed6c2b5644f13ba7e61e8
EmreUstaoglu <55128324+EmreUstaoglu@users.noreply.github.com>
2020-03-12 14:28:30 +0100

    sprint 5 emre
    alle sprint 5 opdrachten

---
+++ emre/Sprint 5/Objecten/Beslissingsstructuren/Opgave 56/java.js
@@ -0,0 +1,7 @@
+postcode = prompt("Vul hier uw postcode in : 1234 XP");
+
+if (postcode == "1010ST"){
+    alert("Kans is 90%");
+}else{
+    alert("Kans is 0,069%");
+}
---
+++ emre/Sprint 5/Objecten/Beslissingsstructuren/lab09/java.js
@@ -0,0 +1,30 @@
+ var rol = prompt('wat is je rol?');
+var inlog = prompt('wat is je inlogcode?')
+
+    switch(rol)
+    {
+    case "admin":
+    if(inlog =='admin')
+    {
+        alert('welkom admin')
+    }
+    else
+    {
+        alert('code ongeldig')
+    }
+        break;
+
+    case "student":
+    if(inlog =='student')
+    {
+        alert('welkom student')
+    }
+    else
+    {
+        alert('code ongeldig')
+    }
+        break;
+
+        default:
+        alert('code ongeldig')
+    }
